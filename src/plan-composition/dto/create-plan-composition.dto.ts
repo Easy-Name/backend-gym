fd90f105743e677f374c07c97c008b932fb40411
oluwaseyi.longe <oluwaseyi.longe@consorciomagalu.com.br>
2025-02-16 23:12:12 -0300

    add ApiProperty to DTO

--- src/plan-composition/dto/create-plan-composition.dto.ts
+++ src/plan-composition/dto/create-plan-composition.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsPositive,
@@ -7,26 +8,32 @@ import {
 } from 'class-validator';
 
 export class CreatePlanCompositionDto {
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   trainingPlanId: number;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   exerciseId: number;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   numberOfSets: number;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   numberOfRepetitions: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   observations?: string;
 
+  @ApiProperty()
   @IsString()
   @Length(1, 1) // Ensures it's a single character (e.g., 'A', 'B', 'C')
   day: string;
