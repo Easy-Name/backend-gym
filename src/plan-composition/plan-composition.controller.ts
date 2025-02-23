4d3374a1393292c0804a42dbe720a139ec03e851
oluwaseyi.longe <oluwaseyi.longe@consorciomagalu.com.br>
2025-02-23 18:04:38 -0300

    swagger hotfix2

--- src/plan-composition/plan-composition.controller.ts
+++ src/plan-composition/plan-composition.controller.ts
@@ -13,6 +13,7 @@ import { CreatePlanCompositionDto } from './dto/create-plan-composition.dto';
 import { UpdatePlanCompositionDto } from './dto/update-plan-composition.dto';
 import { CreateManyPlanCompositionDto } from './dto/create-many-plan-composition.dto';
 import { Professor } from 'src/auth/decorators/professor.decorator';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('plan-composition')
 export class PlanCompositionController {
@@ -38,6 +39,13 @@ export class PlanCompositionController {
   }
 
   @Get('user/:userId') // Define the route with `id` as a path parameter
+  @ApiQuery({
+    // Define the `status` query parameter in Swagger
+    name: 'status',
+    required: false, // Mark the parameter as optional
+    description: 'Filter by status (e.g., ACTIVE, INACTIVE)',
+    example: 'ACTIVE', // Provide an example value
+  })
   async findAllByUser(
     @Param('id') id: string, // Extract `id` from the path
     @Query('status') status: string = 'ACTIVE', // Extract `status` from the query string, default to 'ACTIVE'
